472cedc74e19566a6464da0a427d4ba616a0f79b
Pablo Martínez <pabloextreme27@gmail.com>
2024-12-25 14:20:02 +0100

    First version of the hub

---
+++ app/config/constants.js
@@ -0,0 +1,37 @@
+export const resources = [
+  {
+    name: "Pixelcut AI - Upscaler",
+    description: "Upscaler de imagenes gracias a IA",
+    url: "https://www.pixelcut.ai/t/upscaler",
+    icon: "https://www.pixelcut.ai/favicon.ico",
+    categories: ["Imágen", "IA"],
+  },
+  {
+    name: "Codi Link",
+    description: "Editor de código online de HTML, JS, y CSS",
+    url: "https://codi.link",
+    icon: "https://code.visualstudio.com/assets/apple-touch-icon.png",
+    categories: ["Programación"],
+  },
+  {
+    name: "Simulador móvil",
+    description: "Simulador de smartphones y tablets muy realista.",
+    url: "https://chromewebstore.google.com/detail/simulador-m%C3%B3vil-herramien/ckejmhbmlajgoklhgbapkiccekfoccmk?hl=es",
+    icon: "https://lh3.googleusercontent.com/AdZGgDWDCE3zyp1t_rJo1VT_9ge6l7QeFftWKrr2opJc4IgOmbQY82_4s44rGpHqZjw6ALtEsjlY8Sv1nERN-nAJEA=s120",
+    categories: ["Chrome"],
+  },
+  {
+    name: "es6-string-html",
+    description: "Interpreta HTML en query strings de javascript.",
+    url: "https://marketplace.visualstudio.com/items?itemName=Tobermory.es6-string-html",
+    icon: "https://tobermory.gallerycdn.vsassets.io/extensions/tobermory/es6-string-html/2.17.0/1733307395603/Microsoft.VisualStudio.Services.Icons.Default",
+    categories: ["VS Code"],
+  },
+  {
+    name: "Photopea",
+    description: "Clon de Photoshop gratuito online.",
+    url: "https://www.photopea.com/",
+    icon: "https://www.photopea.com/promo/icon512.png",
+    categories: ["Imágen"],
+  },
+];
---
+++ app/setup.js
@@ -0,0 +1,118 @@
+import { Component } from "https://cdn.devetty.es/ScopeJS/js";
+import { resources } from "./config/constants.js";
+
+Component({
+  controller: function () {
+    const categories = [...new Set(resources.flatMap((resource) => resource.categories))];
+
+    this.filters = {
+      search: "",
+      categories: {
+        ...categories.reduce((acc, category) => ({ ...acc, [category]: false }), {}),
+      },
+    };
+
+    this.selectCategory = (category) => {
+      this.filters.categories = {
+        ...this.filters.categories,
+        [category]: !this.filters.categories[category],
+      };
+      this.apply();
+    };
+
+    this.search = (event) => {
+      this.filters.search = event.target.value;
+      this.apply();
+    };
+
+    this.filterResources = () => {
+      const any_active = Object.values(this.filters.categories).some((value) => value);
+      return resources.filter((resource) => {
+        if (!this.filters.search) {
+          if (!any_active) {
+            return true;
+          }
+          let active = false;
+          for (const category of resource.categories) {
+            if (this.filters.categories[category]) {
+              active = true;
+              break;
+            }
+          }
+          return active;
+        }
+
+        if (!any_active) {
+          return resource.name.toLowerCase().includes(this.filters.search.toLowerCase()) || resource.description.toLowerCase().includes(this.filters.search.toLowerCase());
+        }
+
+        let active = false;
+        if (resource.name.toLowerCase().includes(this.filters.search.toLowerCase()) || resource.description.toLowerCase().includes(this.filters.search.toLowerCase())) {
+          for (const category of resource.categories) {
+            if (this.filters.categories[category]) {
+              active = true;
+              break;
+            }
+          }
+        }
+
+        return active;
+      });
+    };
+  },
+  render: function () {
+    return /* HTML */ `
+      <header class="section-header">
+        <div class="container">
+          <h1>Hub de Utilidades</h1>
+          <p>Las mejores herramientas para optimizar tu flujo de trabajo, al alcance de un clic.</p>
+
+          <form>
+            <input oninput="search()" type="search" placeholder="Buscar..." />
+          </form>
+
+          <nav>
+            <ul>
+              ${Object.keys(this.filters.categories)
+                .map((category) => /* HTML */ ` <li onclick="selectCategory('${category}')" ${this.filters.categories[category] ? "class='active'" : ""}>${category}</li> `)
+                .join("")}
+            </ul>
+          </nav>
+        </div>
+      </header>
+
+      <main>
+        <div class="container">
+          ${this.filterResources().length === 0 ? /* HTML */ `<p style="text-align: center; padding: 2rem 0">No se encontraron recursos</p>` : ""}
+
+          <ul class="resources">
+            ${this.filterResources()
+              .map(
+                (resource) => /* HTML */ `
+                  <li class="resource">
+                    <img src="${resource.icon}" alt="${resource.name}" />
+                    <div>
+                      <h3>${resource.name}</h3>
+                      <p>${resource.description}</p>
+                      <ul>
+                        ${resource.categories.map((category) => /* HTML */ `<li onclick="selectCategory('${category}')" ${this.filters.categories[category] ? "class='active'" : ""}>${category}</li>`).join("")}
+                      </ul>
+                    </div>
+                    <div style="flex: 1"></div>
+                    <a href="${resource.url}" target="_blank"> Ver </a>
+                  </li>
+                `
+              )
+              .join("")}
+          </ul>
+        </div>
+      </main>
+
+      <footer>
+        <a href="https://github.com/PabloTheBlink/HUB" target="_blank">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub" />
+        </a>
+      </footer>
+    `;
+  },
+}).render(document.body);
